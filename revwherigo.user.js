4507265ed6fc63eff9673542d61bb931ac46b8ac
Avogado <avogado.gc@gmail.com>
2020-01-01 09:57:26 +0100

    Port to tempermonkey

--- revwherigo.user.js
+++ revwherigo.user.js
@@ -5,7 +5,9 @@
 // @version         0.2
 // @include         http://www.geocaching.com/geocache/*
 // @include         https://www.geocaching.com/geocache/*
-// @grant           none
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @grant           GM.xmlHttpRequest
+// @connect         gc.de
 // ==/UserScript==
 
 function revwhereigo_crack() {
@@ -15,23 +17,27 @@ function revwhereigo_crack() {
         console.log('revwhereigo: Too few or too many codes found' + allCodes)
         allCodes = allCodes.slice(0, 3)
     }
-    var codes = allCodes.join(' ')
+    var codes = allCodes.join('+')
 
     // $ curl 'https://gc.de/async.php' --data 'decoded=104143 577896 165623&key=&encoded=&method=reversewherigo&decode=&encode=1'
     // {"decoded":"104143 577896 165623","key":"","encoded":"N 49\u00b0 45.674 E 013\u00b0 51.280","method":"reversewherigo","replace_encoded":0,"message":""}
-    $.post(
-        'https://gc.de/async.php',
-        {method: 'reversewherigo', decoded: codes, encode: 1},
-        function(data, status) {
+    GM.xmlHttpRequest({
+        method: 'POST',
+        url: 'https://gc.de/async.php',
+        data: 'method=reversewherigo&decoded=' + codes + '&encode=1',
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        onload: function(response) {
+            var data = JSON.parse(response.responseText)
             if (data.message != '') {
-                var msg = 'revwhereigo: Failed to decode the codes: ' + codes + '. "' + data.message + '"'
-                console.log(msg)
+                console.log('revwhereigo: Failed to decode the codes: ' + codes + '. "' + data.message + '"')
             } else {
                 console.log("revwhereigo: Updating cache note with " + data.encoded)
                 revwhereigo_update_note(data.encoded)
             }
         }
-    );
+    });
     return false
 }
 
